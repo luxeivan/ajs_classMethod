77f8ec308044f28beff3d39eae7086722d8ff991
luxeivan <luxeivan@gmail.com>
2022-04-02 11:45:53 +0300

    commplete jest

--- src/js/Character.js
+++ src/js/Character.js
@@ -7,4 +7,20 @@ export default class Character {
     this.health = health;
     this.level = level;
   }
+
+  levelUp() {
+    if (this.health === 0) {
+      throw new Error('нельзя повысить левел умершего');
+    }
+    this.level += 1;
+    this.attack *= 0.2;
+    this.defence *= 0.2;
+    this.health = 100;
+  }
+
+  damage(points) {
+    if (this.health >= 0) {
+      this.health -= points * (1 - this.defence / 100)
+    }
+  }
 }
--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -12,7 +12,60 @@ test('Character norm', () => {
 
 test('Character not norm', () => {
   const result = () => {
-     new Character('D', 90, 3) 
-};
+    new Character('D', 90, 3)
+  };
   expect(result).toThrow('Имя должно быть от 2 до 10 символов');
+});
+
+test('Character function levelUp', () => {
+  const result = new Character('Devil', 90, 3);
+  result.attack = 25;
+  result.defence = 25;
+  const tobe = {
+    name: 'Devil',
+    health: 100,
+    level: 4,
+    defence: 25 * 0.2,
+    attack: 25 * 0.2
+  };
+  result.levelUp();
+  expect(result).toEqual(tobe);
+});
+
+test('Character function levelUp not norm', () => {
+  const result = () => {
+    const ch = new Character('Devil', 0, 3);
+    ch.levelUp();
+  };
+  expect(result).toThrow('нельзя повысить левел умершего');
+});
+
+test('Character function damage', () => {
+  const result = new Character('Devil', 90, 3);
+  result.attack = 25;
+  result.defence = 25;
+  const tobe = {
+    name: 'Devil',
+    health: 90 - 20 * (1 - 25 / 100),
+    level: 3,
+    defence: 25,
+    attack: 25
+  };
+  result.damage(20);
+  expect(result).toEqual(tobe);
+});
+
+test('Character function damage', () => {
+  const result = new Character('Devil', -1, 3);
+  result.attack = 25;
+  result.defence = 25;
+  const tobe = {
+    name: 'Devil',
+    health: -1,
+    level: 3,
+    defence: 25,
+    attack: 25
+  };
+  result.damage(20);
+  expect(result).toEqual(tobe);
 });
